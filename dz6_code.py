daa973a2d1215bb8d532c6bf96a176c2676eff7b
DrobotTS <90732395+DrobotTS@users.noreply.github.com>
2021-11-26 10:03:57 +0200

    update

--- dz6_code.py
+++ dz6_code.py
@@ -3,5 +3,4 @@ from dz_6_class_file import DataInfo
 data_result = DataInfo("domains.txt", "names.txt", "authors.txt")
 dom_res = data_result.domain_data()
 sur_res = data_result.name_data()
-date_res = data_result.date_data()
-print(data_result, dom_res, sur_res, date_res, sep="\n")
+print(dom_res, sur_res, data_result, sep="\n")
